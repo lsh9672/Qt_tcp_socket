92d92396ab56042a9120bb99be5ff50dec0aceb3
leesh <lsh80165@gmail.com>
2021-07-22 09:30:23 +0900

    v1

--- ThreadSocketServer/mainwindow.cpp
+++ ThreadSocketServer/mainwindow.cpp
@@ -10,7 +10,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    this->setWindowTitle("Server");
+    this->setWindowTitle("TCP Server");
 
     qDebug() << "ui address"<< ui;
 
--- ThreadSocketServer/myserver.h
+++ ThreadSocketServer/myserver.h
@@ -4,6 +4,7 @@
 #include "myThread.h"
 
 
+
 class MyServer : public QTcpServer
 {
 
